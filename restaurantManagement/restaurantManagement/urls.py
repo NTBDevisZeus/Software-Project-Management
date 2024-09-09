cb063f2d4e03444392558fa7f3e16189f4a869c1
NTBDavidZeus <nguyenthaibinhpy2019@gmail.com>
2024-09-09 23:10:10 +0700

    udpat

--- restaurantManagement/restaurantManagement/urls.py
+++ restaurantManagement/restaurantManagement/urls.py
@@ -1,12 +1,9 @@
-from django.conf import settings
+
 from django.contrib import admin
 from django.urls import path, re_path, include
-from restaurant.views import CreateOrderView, StatisticsView, StatisticsByProductView, OrderListView, OrderDeleteView, \
-    OrderDetailView, OrderCreateView, OrderUpdateView, ReservationCreateView, ReservationListView, \
-    ReservationDetailView, ReservationUpdateView, ReservationDeleteView, PaymentListCreateView, PaymentDetailView, \
-    ProductListCreateView, ProductDetailView, UserViewSet
 
-from rest_framework.routers import DefaultRouter
+
+
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
